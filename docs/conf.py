56d740cd69f734ef889b727bc32501c0987baf55
Madelyn Broome <mbroome9@gmail.com>
2025-08-21 10:01:32 -0700

    RTD debugging

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../wind_ae/'))
-sys.path.insert(0, os.path.abspath('../../wind_ae/wrapper/'))
-sys.path.insert(0, os.path.abspath('../../wind_ae/wrapper/wrapper_utils/'))
 
 # sys.path.insert(0, os.path.abspath('../wind_ae/'))
 # sys.path.insert(0, os.path.abspath('../wind_ae/wrapper/'))
