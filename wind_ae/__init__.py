dd4bcaf6a641d789f257366df58d95b5a7a1be29
Madelyn Broome <mbroome9@gmail.com>
2025-08-20 22:17:41 -0700

    Clearing defunct version

--- wind_ae/__init__.py
+++
@@ -1,4 +0,0 @@
-# import McAstro as McAstro
-# from McAstro.atoms import atomic_species as McAtom 
-# from .McAstro.mcastro_file import McAstro
-from wind_ae.wrapper.relax_wrapper import wind_simulation as wind_sim
